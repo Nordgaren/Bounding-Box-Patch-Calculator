3a63c009d06122728cb052c77ad45c4060b25111
nordgaren <nordgarentv@gmail.com>
2022-06-29 02:37:22 -0700

    Solved, again. :fatcat:

--- Bounding Box Patch Calculator/BoundingBoxSolver.cs
+++ Bounding Box Patch Calculator/BoundingBoxSolver.cs
@@ -1,209 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using HKX2;
+using SoulsAssetPipeline.FLVERImporting;
 using SoulsFormats;
-using SharpDX;
-
 
 namespace Bounding_Box_Patch_Calculator
 {
-    //This code is from FBX2FLVER. Thank Meowmaritus. 
-    public class BoundingBoxSolver
+    public static class BoundingBoxSolver
     {
-        //private readonly FBX2FLVERImporter Importer;
-        //public BoundingBoxSolver(FBX2FLVERImporter Importer)
-        //{
-        //    this.Importer = Importer;
-        //}
-
-        private bool UseDirectBoneIndices = false;
-
-        public BoundingBoxSolver(bool useDirectBoneIndices)
+        public static void FixAllBoundingBoxes(FLVER2 flver)
         {
-            UseDirectBoneIndices = useDirectBoneIndices;
-        }
-
-        private Dictionary<FLVER.Vertex, List<FLVER.Bone>> PrecalculatedBoneLists = new Dictionary<FLVER.Vertex, List<FLVER.Bone>>();
-
-        private List<FLVER.Bone> GetAllBonesReferencedByVertex(FLVER2 f, FLVER2.Mesh m, FLVER.Vertex v)
-        {
-            if (!PrecalculatedBoneLists.ContainsKey(v))
+            flver.Header.BoundingBoxMin = new System.Numerics.Vector3();
+            flver.Header.BoundingBoxMax = new System.Numerics.Vector3();
+            foreach (FLVER.Bone bone in flver.Bones)
             {
-                List<FLVER.Bone> result = new List<FLVER.Bone>();
+                bone.BoundingBoxMin = new System.Numerics.Vector3();
+                bone.BoundingBoxMax = new System.Numerics.Vector3();
+            }
 
-                foreach (var vbi in v.BoneIndices)
+            for (int i = 0; i < flver.Meshes.Count; i++)
+            {
+
+                FLVER2.Mesh mesh = flver.Meshes[i];
+                mesh.BoundingBox = new FLVER2.Mesh.BoundingBoxes();
+
+                foreach (FLVER.Vertex vertex in mesh.Vertices)
                 {
-                    var vertBoneIndex = vbi;
+                    flver.Header.UpdateBoundingBox(vertex.Position);
+                    if (mesh.BoundingBox != null)
+                        mesh.UpdateBoundingBox(vertex.Position);
 
-                    if (vertBoneIndex >= 0)
+                    for (int j = 0; j < vertex.BoneIndices.Length; j++)
                     {
-                        if (m.Dynamic == 0)
-                            vertBoneIndex = v.NormalW;
+                        var boneIndex = vertex.BoneIndices[j];
+                        var boneDoesNotExist = false;
 
-                        if (vertBoneIndex >= f.Bones.Count() || (vertBoneIndex >= m.BoneIndices.Count() && vertBoneIndex >= v.BoneIndices.Length))
-                            continue;
-
-                        if (UseDirectBoneIndices)
+                        // Mark bone as not-dummied-out since there is geometry skinned to it.
+                        if (boneIndex >= 0 && boneIndex < flver.Bones.Count)
                         {
-                            result.Add(f.Bones[vertBoneIndex]);
+                            flver.Bones[boneIndex].Unk3C = 0;
                         }
                         else
                         {
-                            if (m.BoneIndices.Count() > 0 && m.BoneIndices[vertBoneIndex] >= 0)
-                                result.Add(f.Bones[m.BoneIndices[vertBoneIndex]]);
-                            else if (v.BoneIndices.Length > 0 && v.BoneIndices[vertBoneIndex] >= 0)
-                                result.Add(f.Bones[vertBoneIndex]);
+                            //Logger.LogWarning($"Vertex skinned to bone '{bone.Name}' which does NOT exist in the skeleton.");
+                            boneDoesNotExist = true;
                         }
+
+                        if (!boneDoesNotExist)
+                            flver.Bones[boneIndex].UpdateBoundingBox(flver.Bones, vertex.Position);
                     }
                 }
 
-                PrecalculatedBoneLists.Add(v, result);
             }
-
-            return PrecalculatedBoneLists[v];
-        }
-
-        private List<FLVER.Vertex> GetVerticesParentedToBone(FLVER2 f, FLVER.Bone b)
-        {
-            var result = new List<FLVER.Vertex>();
-            foreach (var sm in f.Meshes)
-            {
-                foreach (var v in sm.Vertices)
-                {
-                    var bonesReferencedByThisShit = GetAllBonesReferencedByVertex(f, sm, v);
-                    if (bonesReferencedByThisShit.Contains(b))
-                        result.Add(v);
-                }
-            }
-            return result;
-        }
-
-        private BoundingBox GetBoundingBox(Vector3[] verts)
-        {
-            if (verts.Length > 0)
-                return BoundingBox.FromPoints(verts);
-
-            return new BoundingBox(Vector3.Zero, Vector3.Zero);
-        }
-
-        Matrix GetParentBoneMatrix(FLVER2 f, FLVER.Bone bone)
-        {
-            FLVER.Bone parent = bone;
-
-            var boneParentMatrix = Matrix.Identity;
-
-            do
-            {
-                boneParentMatrix *= Matrix.Scaling(parent.Scale.X, parent.Scale.Y, parent.Scale.Z);
-                boneParentMatrix *= Matrix.Scaling(parent.Rotation.X);
-                boneParentMatrix *= Matrix.RotationZ(parent.Rotation.Z);
-                boneParentMatrix *= Matrix.RotationY(parent.Rotation.Y);
-
-                //boneParentMatrix *= Matrix.CreateRotationZ(parent.EulerRadian.Z);
-                //boneParentMatrix *= Matrix.CreateRotationX(parent.EulerRadian.X);
-                //boneParentMatrix *= Matrix.CreateRotationY(parent.EulerRadian.Y);
-                boneParentMatrix *= Matrix.Translation(parent.Translation.X, parent.Translation.Y, parent.Translation.Z);
-                //boneParentMatrix *= Matrix.CreateScale(parent.Scale);
-
-                if (parent.ParentIndex >= 0)
-                {
-                    parent = f.Bones[parent.ParentIndex];
-                }
-                else
-                {
-                    parent = null;
-                }
-            }
-            while (parent != null);
-
-            return boneParentMatrix;
-        }
-
-        float Multiplier = 2.25f;
-
-        public void SetMuliplier(float multiplier)
-        {
-            var lol = Matrix.Identity;
-            Multiplier = multiplier;
-        }
-
-        private void SetBoneBoundingBox(FLVER2 f, FLVER.Bone b)
-        {
-            var multiplierVector = new System.Numerics.Vector3(Multiplier, Multiplier, Multiplier);
-            var bb = GetBoundingBox(GetVerticesParentedToBone(f, b).Select(v => new Vector3(v.Position.X, v.Position.Y, v.Position.Z)).ToArray());
-            if (bb.Maximum.LengthSquared() != 0 || bb.Minimum.LengthSquared() != 0)
-            {
-                var matrix = GetParentBoneMatrix(f, b);
-                b.BoundingBoxMin = Vector3.Transform(bb.Minimum, Matrix.Invert(matrix)).ToNumerics();
-                b.BoundingBoxMax = Vector3.Transform(bb.Maximum, Matrix.Invert(matrix)).ToNumerics();
-                b.BoundingBoxMin = System.Numerics.Vector3.Multiply(b.BoundingBoxMin, multiplierVector);
-                b.BoundingBoxMax = System.Numerics.Vector3.Multiply(b.BoundingBoxMax, multiplierVector);
-            }
-            else
-            {
-                b.BoundingBoxMin = new System.Numerics.Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-                b.BoundingBoxMax = new System.Numerics.Vector3(float.MinValue, float.MinValue, float.MinValue);
-            }
-        }
-
-        public void FixAllBoundingBoxes(FLVER2 f)
-        {
-            PrecalculatedBoneLists.Clear();
-
-            foreach (var b in f.Bones)
-            {
-                var boneParentMatrix = Matrix.Scaling(b.Rotation.X);
-                SetBoneBoundingBox(f, b);
-
-            }
-
-            var submeshBBs = new List<BoundingBox>();
-
-            foreach (var sm in f.Meshes)
-            {
-                sm.BoundingBox = new FLVER2.Mesh.BoundingBoxes();
-                var bb = GetBoundingBox(sm.Vertices.Select(v => new Vector3(v.Position.X, v.Position.Y, v.Position.Z)).ToArray());
-                if (bb.Maximum.LengthSquared() != 0 || bb.Minimum.LengthSquared() != 0)
-                {
-                    submeshBBs.Add(bb);
-                    sm.BoundingBox.Min = bb.Minimum.ToNumerics();
-                    sm.BoundingBox.Max = bb.Maximum.ToNumerics();
-                }
-                else
-                {
-                    sm.BoundingBox.Min = new System.Numerics.Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-                    sm.BoundingBox.Max = new System.Numerics.Vector3(float.MinValue, float.MinValue, float.MinValue);
-                    //sm.BoundingBox = null;
-                }
-            }
-
-            if (submeshBBs.Count > 0)
-            {
-                var finalBB = submeshBBs[0];
-                for (int i = 1; i < submeshBBs.Count; i++)
-                {
-                    finalBB = BoundingBox.Merge(finalBB, submeshBBs[i]);
-                }
-
-                var multiplierVector = new System.Numerics.Vector3(Multiplier, Multiplier, Multiplier);
-                f.Header.BoundingBoxMin = new System.Numerics.Vector3(finalBB.Minimum.X, finalBB.Minimum.Y, finalBB.Minimum.Z);
-                f.Header.BoundingBoxMax = new System.Numerics.Vector3(finalBB.Maximum.X, finalBB.Maximum.Y, finalBB.Maximum.Z);
-                f.Header.BoundingBoxMin = System.Numerics.Vector3.Multiply(f.Header.BoundingBoxMin, multiplierVector);
-                f.Header.BoundingBoxMax = System.Numerics.Vector3.Multiply(f.Header.BoundingBoxMax, multiplierVector);
-
-
-            }
-            else
-            {
-                f.Header.BoundingBoxMin = new System.Numerics.Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-                f.Header.BoundingBoxMax = new System.Numerics.Vector3(float.MinValue, float.MinValue, float.MinValue);
-
-            }
-
-
-
         }
     }
 }
--- Bounding Box Patch Calculator/Program.cs
+++ Bounding Box Patch Calculator/Program.cs
@@ -16,7 +16,7 @@ namespace Bounding_Box_Patch_Calculator
         static void Main(string[] args)
         {
 #if DEBUG
-            args = new string[] { @"G:\Steam\steamapps\common\ELDEN RING 1.04.2\Game\parts\wp_a_0919.partsbnd.dcx" };
+            args = new string[] { @"G:\Steam\steamapps\common\ELDEN RING 1.05\Game\parts\wp_a_0617.partsbnd.dcx" };
 #endif
             if (args.Length == 0)
             {
@@ -31,86 +31,89 @@ namespace Bounding_Box_Patch_Calculator
                 }
             }
 
-            Console.WriteLine("This program just recalculates the bounding box of each mesh's bones using the Bounding Box Solver from FBX2FLVER.");
-            Console.WriteLine("To use this patcher, move the files you're trying to patch to a new folder and run this exe inside that folder.");
-            Console.WriteLine("Most of the time you DO NOT want to use direct bone indicies, from what I can tell, but I made it an option.");
+            //Console.WriteLine("This program just recalculates the bounding box of each mesh's bones using the Bounding Box Solver from FBX2FLVER.");
+            //Console.WriteLine("To use this patcher, move the files you're trying to patch to a new folder and run this exe inside that folder.");
+            //Console.WriteLine("Most of the time you DO NOT want to use direct bone indicies, from what I can tell, but I made it an option.");
+            Console.WriteLine("This program just recalculates the bounding box of each mesh's bones using the Bounding Box code from:");
+            Console.WriteLine("SoulsAssetPipeline by Meowmaritus");
             Console.WriteLine();
 
-            Console.WriteLine("Use direct bone indicies? [Y/N] (Change this if you don't get desired output)");
-            var useDirectBoneIndicies = YesOrNo();
+            //Console.WriteLine("Use direct bone indicies? [Y/N] (Change this if you don't get desired output)");
+            //var useDirectBoneIndicies = YesOrNo();
 
-            Console.WriteLine("Would you like to use a custom bounding box multiplier? [Y/N] (Default is 2.25)");
-            var useCustomMultiplier = YesOrNo();
+            //Console.WriteLine("Would you like to use a custom bounding box multiplier? [Y/N] (Default is 2.25)");
+            //var useCustomMultiplier = YesOrNo();
 
-            float multiplier = 2.25f;
-            if (useCustomMultiplier)
-            {
-                Console.WriteLine("Enter a custom multiplier (eg 1.5)");
-                var input = Console.ReadLine();
-                var tryParse = float.TryParse(input, out multiplier);
+            //float multiplier = 2.25f;
+            //if (useCustomMultiplier)
+            //{
+            //    Console.WriteLine("Enter a custom multiplier (eg 1.5)");
+            //    var input = Console.ReadLine();
+            //    var tryParse = float.TryParse(input, out multiplier);
 
-                if (!tryParse)
-                {
-                    Console.WriteLine("Invalid Multiplier. Using default");
-                    multiplier = 2.25f;
-                }
-            }
+            //    if (!tryParse)
+            //    {
+            //        Console.WriteLine("Invalid Multiplier. Using default");
+            //        multiplier = 2.25f;
+            //    }
+            //}
 
-            PatchFiles(useDirectBoneIndicies, args, multiplier);
+            PatchFiles(args);
+            Console.WriteLine("Press the Any Key to Terminate Program");
+            Console.ReadKey();
         }
 
-        private static void PatchFiles(bool useDirectBoneIndicies, string[] parts, float multiplier)
+        private static void PatchFiles(string[] parts)
         {
-            var boundingBoxSolver = new BoundingBoxSolver(useDirectBoneIndicies);
-            boundingBoxSolver.SetMuliplier(multiplier);
-
             foreach (var file in parts)
             {
+                Console.WriteLine(file);
+
+                if (!File.Exists($"{file}.bak"))
+                    File.Copy(file, $"{file}.bak");
+
                 if (file.Contains(".flver"))
-                    PatchFlver(boundingBoxSolver, file, FLVER2.Read(file));
+                    PatchFlver(file, FLVER2.Read(file));
 
                 if (!file.Contains(".partsbnd"))
                     continue;
 
                 if (BND4.IsRead(file, out BND4 partBND4))
                 {
-                    PatchParts(boundingBoxSolver, file, partBND4);
+                    PatchParts(partBND4);
                     partBND4.Write(file);
                 }
                 else if (BND3.IsRead(file, out BND3 partBND3))
                 {
-                    PatchParts(boundingBoxSolver, file, partBND3);
+                    PatchParts(partBND3);
                     partBND3.Write(file);
                 }
 
             }
         }
 
-        private static void PatchParts(BoundingBoxSolver boundingBoxSolver, string file, IBinder ogPart)
+        private static void PatchParts(IBinder part)
         {
-            Console.WriteLine(file);
+        
 
-            if (!File.Exists($"{file}.bak"))
-                File.Copy(file, $"{file}.bak");
-
-            for (int i = 0; i < ogPart.Files.Count; i++)
+            for (int i = 0; i < part.Files.Count; i++)
             {
-                if (ogPart.Files[i].Name.EndsWith(".flver"))
+                if (part.Files[i].Name.EndsWith(".flver"))
                 {
-                    var ogFlver = FLVER2.Read(ogPart.Files[i].Bytes);
+                    var flver = FLVER2.Read(part.Files[i].Bytes);
 #if DEBUG
-                    PrintDebugInfo(file, ogFlver);
+                    PrintDebugInfo(flver);
 #endif
-                    boundingBoxSolver.FixAllBoundingBoxes(ogFlver);
+                    BoundingBoxSolver.FixAllBoundingBoxes(flver);
 #if DEBUG
-                    PrintDebugInfo(file, ogFlver);
+                    PrintDebugInfo(flver);
 #endif
-                    ogPart.Files[i].Bytes = ogFlver.Write();
+                    part.Files[i].Bytes = flver.Write();
                 }
             }
         }
 
-        private static void PatchFlver(BoundingBoxSolver boundingBoxSolver, string file, FLVER2 ogFlver)
+        private static void PatchFlver(string file, FLVER2 flver)
         {
             Console.WriteLine(file);
 
@@ -118,27 +121,27 @@ namespace Bounding_Box_Patch_Calculator
                 File.Copy(file, $"{file}.bak");
 
 #if DEBUG
-            PrintDebugInfo(file, ogFlver);
+            PrintDebugInfo(flver);
 #endif
 
-            boundingBoxSolver.FixAllBoundingBoxes(ogFlver);
+            BoundingBoxSolver.FixAllBoundingBoxes(flver);
 #if DEBUG
-            PrintDebugInfo(file, ogFlver);
+            PrintDebugInfo(flver);
 #endif
-            ogFlver.Write(file);
+            flver.Write(file);
         }
 
 #if DEBUG
-        private static void PrintDebugInfo(string file, FLVER2 ogFlver)
+        private static void PrintDebugInfo(FLVER2 flver)
         {
             Console.WriteLine();
-            Console.WriteLine($"{file}");
+            //Console.WriteLine($"{file}");
             Console.WriteLine("Header");
-            Console.WriteLine($"BBMin: {ogFlver.Header.BoundingBoxMin}");
-            Console.WriteLine($"BBMax: {ogFlver.Header.BoundingBoxMax}");
+            Console.WriteLine($"BBMin: {flver.Header.BoundingBoxMin}");
+            Console.WriteLine($"BBMax: {flver.Header.BoundingBoxMax}");
             Console.WriteLine();
             Console.WriteLine("Bones");
-            foreach (FLVER.Bone bone in ogFlver.Bones)
+            foreach (FLVER.Bone bone in flver.Bones)
             {
                 Console.WriteLine($"{bone.Name}");
                 Console.WriteLine($"BBMin: {bone.BoundingBoxMin}");
@@ -148,7 +151,7 @@ namespace Bounding_Box_Patch_Calculator
             Console.WriteLine();
             Console.WriteLine("Meshes");
             int num = 0;
-            foreach (FLVER2.Mesh mesh in ogFlver.Meshes)
+            foreach (FLVER2.Mesh mesh in flver.Meshes)
             {
                 Console.WriteLine($"{mesh} {num}");
                 Console.WriteLine($"BBMin: {mesh.BoundingBox?.Min}");
